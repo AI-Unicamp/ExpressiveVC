7e1103d0f4a3efacfe489957fa63e01d7fead47a
Lucas Hideki Ueda <lucashueda@gmail.com>
2023-07-13 15:31:05 -0300

    print debug

--- vdecoder/hifigan/models.py
+++ vdecoder/hifigan/models.py
@@ -396,16 +396,20 @@ class Generator_energy(torch.nn.Module):
 
     def forward(self, x, f0, energy, g=None):
         # print(1,x.shape,f0.shape,f0[:, None].shape)
+        print(f"in Hifigan: energy mean = {energy.mean()} pitch mean = {f0.mean()} x mean = {x.mean()}")
+
         f0 = self.f0_upsamp(f0[:, None]).transpose(1, 2)  # bs,n,t
         energy = self.energy_upsamp(energy[:, None]) # bs, t, n
 
-        print(f"in Hifigan: energy mean = {energy.mean()} pitch mean = {f0.mean()} x mean = {x.mean()}")
 
         # print(2,f0.shape, energy.shape)
         har_source, noi_source, uv = self.m_source(f0)
         har_source = har_source.transpose(1, 2)
         x = self.conv_pre(x)
         x = x + self.cond(g)
+
+        print(f"#2 in Hifigan: energy mean = {energy.mean()} pitch mean = {f0.mean()} x mean = {x.mean()} har mean = {har_count.mean()}")
+
         # print(124,x.shape,har_source.shape)
         for i in range(self.num_upsamples):
             x = F.leaky_relu(x, LRELU_SLOPE)
